6fd84d798bd90c6c514ac98dc6da497c93365c1f
SungSeokMin <jkl154527@gmail.com>
2021-08-12 16:12:36 +0900

    🎨 id, like 제거

--- JS/Board/detailBoard.js
+++ JS/Board/detailBoard.js
@@ -5,12 +5,10 @@ makeHeader();
 
 loginControll();
 
-const { title, content, like } = JSON.parse(localStorage.detailInfo);
+const { title, content } = JSON.parse(localStorage.detailInfo);
 
 const titleEL = document.querySelector('.title');
 const contentEL = document.querySelector('.content');
-const likeEL = document.querySelector('.like');
 
 titleEL.textContent = title;
 contentEL.innerHTML = content;
-likeEL.textContent = `좋아요 ${like}`;
