04870a64b82f9f7fb3c1fd4ced7e89c7b3bad4c0
SungSeokMin <jkl154527@gmail.com>
2021-08-16 15:34:13 +0900

    ✨ 페이지 로드시 로컬스토리지의 상세정보 삭제

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,9 @@
+// index.html 즉 메인페이지로 오면 localStorage의 detailInfo 없애기
+// 상세페이지를 빠져나와서는 굳이 localStorage에 상세정보를 가지고 있을 필요가 없다.
+window.addEventListener('load', () => {
+  localStorage.removeItem('detailInfo');
+});
+
 /* 글쓰기 버튼 활성화 비활성화 */
 
 const writeBtn = document.querySelector('.write--btn');
@@ -21,8 +27,8 @@ function appendElement(data) {
     let { id, title, content, like } = data[i];
 
     if (title.length >= 30) title = `${title.substring(0, 30)} ...`;
-    if (content.length >= 80) content = `${content.substring(0, 80)}...`;
-
+    if (content.length >= 80) content = `${content.substring(0, 50)}...`;
+    console.log(content);
     postContent.appendChild(makePost(id, title, content, like));
   }
 }
@@ -37,7 +43,6 @@ function postClick() {
       const { data } = result;
 
       localStorage.setItem('detailInfo', JSON.stringify(data));
-      console.log('실행1');
     });
   }
 }
@@ -90,6 +95,7 @@ myContentBtn.addEventListener('click', async () => {
       if (user === nickname) return post;
     });
 
+    console.log(myPostSort);
     appendElement(myPostSort);
     postClick();
   }
