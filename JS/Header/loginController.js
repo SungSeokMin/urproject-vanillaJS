5455118e0f4ae5331c9870595d72c46019197c60
SungSeokMin <jkl154527@gmail.com>
2021-08-22 17:28:52 +0900

    ✨ 로그인, 로그아웃 서버 연동

--- JS/Header/loginController.js
+++ JS/Header/loginController.js
@@ -18,36 +18,47 @@ function loginControll() {
 
   // 메인 페이지에서 로그인 버튼이 눌렸을 경우
 
-  loginBtn.addEventListener('click', () => {
+  loginBtn.addEventListener('click', async () => {
     signinModal.style.display = 'flex';
   });
 
-  signinBtn.addEventListener('click', () => {
+  signinBtn.addEventListener('click', async () => {
     // axios 요청 보내기 => 로그인 성공 || 실패
 
-    const email = 'jkl1545';
-    const password = '1234';
+    const loginReq = await axios.post(
+      'http://localhost:5000/user/login',
+      {
+        email: emailInput.value,
+        password: passwordInput.value,
+      },
+      { withCredentials: true }
+    );
 
-    if (email !== emailInput.value || password !== passwordInput.value) {
-      alert('사용자 정보를 다시 입력해주세요');
-    } else {
-      // 로그인 버튼 숨기고 로그아웃 버튼 보여주기
+    const { message, nickname } = loginReq.data;
+
+    if (loginReq.status === 200 || message === 'login Ok') {
       loginBtn.style.display = 'none';
       logoutBtn.style.display = 'block';
 
       const info = {
-        user: email,
+        nickname,
         isCheck: true,
       };
       sessionStorage.setItem('loginInfo', JSON.stringify(info));
 
       window.location.href = '/index.html';
+    } else if (message === 'email check') {
+      alert('이메일을 다시 확인해주세요.');
+    } else if (message === 'password check') {
+      alert('비밀번호를 다시 확인해주세요.');
     }
   });
 
-  logoutBtn.addEventListener('click', () => {
+  logoutBtn.addEventListener('click', async () => {
     loginBtn.style.display = 'block';
     logoutBtn.style.display = 'none';
+
+    await axios.get('http://localhost:5000/user/logout', { withCredentials: true });
 
     sessionStorage.removeItem('loginInfo');
     window.location.href = '/index.html';
